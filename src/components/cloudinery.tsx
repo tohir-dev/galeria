bd7615a5374e54bb82cb919bec07805118e34491
tohir_dev <devtohir@gmail.com>
2024-09-18 08:34:04 +0000

    fix type declarations

--- src/components/cloudinery.tsx
+++ src/components/cloudinery.tsx
@@ -3,9 +3,19 @@
 import { CldImage, CldUploadButton } from "next-cloudinary";
 import { Button } from "./ui/button";
 import { useRouter } from "next/navigation";
-
-export function CldImg(props: any) {
-  return <div className=""><CldImage {...props} /></div> ;
+export function CldImg(props: { width: string;
+                             height: string;
+                             src:string;
+                             alt:string}) { 
+  const { width, height, ...restProps } = props;
+  return (
+    <div className="">
+      <CldImage 
+        {...restProps}
+        width={parseInt(width)} 
+        height={parseInt(height)} /> 
+    </div> 
+  );
 }
 
 export function CldUploadBtn() {
