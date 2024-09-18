9d4e4729788c1a5b25413f87ed1078613ea06cda
tohir_dev <devtohir@gmail.com>
2024-09-18 08:17:54 +0000

    update header layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,15 @@
-import { Button } from "./ui/button";
+
 import { CldUploadBtn } from "./cloudinery";
 
 export default function Header() {
   return (
     <div className="container mx-auto">
       <div className="flex h-16 items-center px-4">
-        galeria
+        <h1 className="text-2xl">galeria</h1>
         <div className="ml-auto flex items-center space-x-4">
-          <Button asChild>
+
           <CldUploadBtn/>
-          </Button>
+
           <a href="">twitter</a>
         </div>
       </div>
