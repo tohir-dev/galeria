86c9e170356a33153e26db494a000a6f6e134db4
tohir_dev <devtohir@gmail.com>
2024-09-18 08:17:24 +0000

    add fetch images and display in masonry grid layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,30 @@
-
 import { CldImg } from "@/components/cloudinery";
 import cloudinary from "cloudinary";
-import { CldImage } from "next-cloudinary";
 type SearchResult = { public_id: string };
 export default async function Home() {
   const result = (await cloudinary.v2.search
     .expression("resource_type:image")
-    .max_results(10)
-    .execute()) as{resources: SearchResult[]};
+    .sort_by("created_at","desc")
+    .max_results(30)
+    .execute()) as { resources: SearchResult[] };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-2 gap-10 ">
-      <div className="grid grid cols-4 gap-4">
-      {result.resources.map((result) => (<CldImg key={result.public_id} publicId={result.public_id}/>
-      ))}</div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-2 gap-10">
+      <div className="columns-2xs w-full ">
+        {result.resources.map((result) => (
+         <div
+         key={result.public_id}
+         className="mb-4"
+       >
+          <CldImg         
+            width="500"
+            height="500"
+            src={result.public_id}
+            alt="galeria images"
+          />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
