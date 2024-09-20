3fb5062dee9b8072cf03559a6c504987fe2b79d7
tohir_dev <devtohir@gmail.com>
2024-09-20 16:25:07 +0000

    test component

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,3 +1,13 @@
-export default function  Gallery(){
-    
+import Image from "next/image";
+export default function Gallery() {
+  return (
+    <div className="columns-2xs w-full ">
+      <Image
+        alt=""
+        src="https://res.cloudinary.com/dgpnb2lv4/image/upload/v1726846407/freddiejacobart1680174105444724736_lmuxok.jpg"
+        layout="fill"
+        objectFit="cover"
+      />
+    </div>
+  );
 }
