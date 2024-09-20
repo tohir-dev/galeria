7617d7f507f7c51c31b01f3ed02884e9144be4f4
tohir_dev <devtohir@gmail.com>
2024-09-20 16:34:06 +0000

    image updates

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -2,12 +2,16 @@ import Image from "next/image";
 export default function Gallery() {
   return (
     <div className="columns-2xs w-full ">
-      <Image
-        alt=""
-        src="https://res.cloudinary.com/dgpnb2lv4/image/upload/v1726846407/freddiejacobart1680174105444724736_lmuxok.jpg"
-        layout="fill"
-        objectFit="cover"
-      />
+      <div className="">
+      <a href="">
+        <Image
+          alt=""
+          src="https://res.cloudinary.com/dgpnb2lv4/image/upload/v1726846407/freddiejacobart1680174105444724736_lmuxok.jpg"
+          width={500}
+          height={500}
+          objectFit="cover"
+        />
+      </a></div>
     </div>
   );
 }
