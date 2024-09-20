0084691012bf78fb7ea116a1ce9f694388eec285
tohir_dev <devtohir@gmail.com>
2024-09-20 14:45:32 +0000

    create supabase client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,40 @@
 import { CldImg } from "@/components/cloudinery";
 import cloudinary from "cloudinary";
 
-import {
-  TabsContent,
-} from "@/components/ui/tabs"
-
-
+import { TabsContent } from "@/components/ui/tabs";
+import { createClient } from "@supabase/supabase-js";
 
 type SearchResult = { public_id: string };
 export default async function Home() {
- 
+  const supabaseAdmin = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+    process.env.SUPPABASE_SERVICE_ROLE_KEY || ""
+  );
+
   return (
-  <div>
-       
-        {["painting", "photography", "digital"].map(async (category) => {const result = (await cloudinary.v2.search
-      .expression(category).sort_by("public_id","desc")
-      .execute()) as { resources: SearchResult[] };
-return(
+    <div>
+      {["painting", "photography", "digital"].map(async (category) => {
+        const result = (await cloudinary.v2.search
+          .expression(category)
+          .sort_by("public_id", "desc")
+          .execute()) as { resources: SearchResult[] };
+        return (
           <TabsContent key={category} value={category}>
-      <div className="columns-2xs w-full ">
-        {result.resources.map((result) => (
-         <div
-         key={result.public_id}
-         className="md:mb-4 mb-2"
-       >
-          <CldImg         
-            width="500"
-            height="500"
-            src={result.public_id}
-            alt="galeria images"
-          />
-          </div>
-        ))}
-      </div></TabsContent>)})}</div>
-  
+            <div className="columns-2xs w-full ">
+              {result.resources.map((result) => (
+                <div key={result.public_id} className="md:mb-4 mb-2">
+                  <CldImg
+                    width="500"
+                    height="500"
+                    src={result.public_id}
+                    alt="galeria images"
+                  />
+                </div>
+              ))}
+            </div>
+          </TabsContent>
+        );
+      })}
+    </div>
   );
 }
