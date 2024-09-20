b00aa08bca9ab2a63a3c5511ab72aaa9ada0ad84
tohir_dev <devtohir@gmail.com>
2024-09-20 15:55:21 +0000

    getstaticprops for images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,19 @@ import cloudinary from "cloudinary";
 import { TabsContent } from "@/components/ui/tabs";
 import { createClient } from "@supabase/supabase-js";
 
-type SearchResult = { public_id: string };
-export default async function Home() {
+export async function getStaticProps() {
   const supabaseAdmin = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL || "",
     process.env.SUPPABASE_SERVICE_ROLE_KEY || ""
   );
 
+  const { data } = await supabaseAdmin.from("images").select("*").order("id");
+
+  return { props: { images: data } };
+}
+
+type SearchResult = { public_id: string };
+export default async function Home() {
   return (
     <div>
       {["painting", "photography", "digital"].map(async (category) => {
