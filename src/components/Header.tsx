21c7f074e4defbd5a915593ac5650ffad317d1ed
tohir_dev <devtohir@gmail.com>
2024-09-22 16:08:13 +0000

    revert header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,3 @@
-import { TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Header() {
   return (
@@ -6,11 +5,7 @@ export default function Header() {
       <div className="flex h-16 items-center px-2 gap-1">
         <h1 className="text-3xl">art-twitter</h1>
         <div className="ml-auto flex items-center space-x-4">
-          <TabsList className="flex mx-auto gap-2">
-            <TabsTrigger value="painting">Paintings</TabsTrigger>
-            <TabsTrigger value="photography">Photography</TabsTrigger>
-            <TabsTrigger value="digital">Digital</TabsTrigger>
-          </TabsList>
+         
         </div>
       </div>
     </div>
