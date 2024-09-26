c7fe73d614934f16f1741e90d2c10f31eb4930fc
tohir <tohirbabs@gmail.com>
2024-09-26 12:20:52 +0100

    new ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,132 @@
-
-import Gallery from "@/components/Gallery";
-
-
+"use client";
+import Image from "next/image";
+import { useState } from "react";
 
 export type SearchResult = { public_id: string };
-export default async function Home() {
+export default function Home() {
+  const [view, setView] = useState(0);
+  console.log(view);
 
-//   const categories = [
-//    "popular",
-//    "painting",
-//    "photograph",
-//    "digital",
-//    "drawing",
-//    "3d",
-//    "sculpture",
-//    "face",
-//    "anime",
-//    "animal",
-//    "retro",
-//    "pen",
-//    "abstract",
-//    "cultural",
-//    "white",
-//    "balck",
-//    "blue",
-//    "red",
-//    "green",
-//    "landscape",
-//  ];
-//   return categories.map(async (category) => {
-//     const result = (await cloudinary.v2.search
-//       .expression(`tags=${category}`)
-//       .execute()) as { resources: SearchResult[] };
-//     return (
-//       <TabsContent key={category} value={category}>
-//         <div className="max-w-screen">
-//           <div className="columns-1 md:columns-3 lg:columns-4 xl:column-5  gap-1 md:gap-2 w-full ">
-//             {result.resources.map((result) => (
-//               <div key={result.public_id} className="md:mb-2 mb-1">
-//                 <CldImg
-//                   width="500"
-//                   height="500"
-//                   src={result.public_id}
-//                   alt="art-twitter images"
-//                 />
-//               </div>
-//             ))}
-//           </div>
-//         </div>
-//       </TabsContent>
-//     );
-//   });
+  // constresult = (await cloudinary.v2.search
+  //   .expression()
+  //   .max_results(50)
+  //   .execute()) as { resources: SearchResult[] };
+  return (
+    <div className="w-screen h-screen relative overflow-hidden">
+      <div className="relative">
+        <div
+          style={
+            view === 1
+              ? {
+                  top: 0,
+                  left: 0,
+                  height: "100vh",
+                  width: "100vw",
+                  zIndex: 10,
+                  opacity: 1,
+                }
+              : {
+                  top: 0,
+                  left: 0,
+                  height: "calc(20vw*5/4)",
+                  width: "20vw",
+                }
+          }
+          className={`${
+            view === 0 ? "opacity-100" : "opacity-0"
+          } flex items-center transition-all duration-500 max-h-screen bg-black absolute`}
+        >
+          <div
+            onClick={() => setView((view) => (view === 1 ? 0 : 1))}
+            className={` aspect-[4/5] mx-auto ${
+              view ? "h-full" : "w-full"
+            } transition-all duration-500`}
+          >
+            <Image
+              fill={true}
+              objectFit="contain"
+              src="https://i.ibb.co/NSbfjRs/mendezmendezart-1837138020157337605.jpg"
+              alt="galeria images"
+              className=""
+            />
+          </div>
+        </div>
+        <div
+          style={
+            view === 2
+              ? {
+                  top: 0,
+                  left: 0,
+                  height: "100vh",
+                  width: "100vw",
+                  zIndex: 10,
+                  opacity: 1,
+                }
+              : {
+                  top: "calc(20vw*5/4)",
+                  left: 0,
+                  height: "20vw",
+                  width: "20vw",
+                }
+          }
+          className={`${
+            view === 0 ? "opacity-100" : "opacity-0"
+          } flex items-center transition-all duration-500 max-h-screen bg-black absolute`}
+        >
+          <div
+            onClick={() => setView((view) => (view === 2 ? 0 : 2))}
+            className={` aspect-[5/5] mx-auto ${
+              view ? "h-full" : "w-full"
+            } transition-all duration-500`}
+          >
+            <Image
+              fill={true}
+              objectFit="contain"
+              src="https://i.ibb.co/JmRyybN/Luca-Ponsato-Art-1838233197378785500.jpg"
+              alt="galeria images"
+              className=""
+            />
+          </div>
+        </div>
+      </div>
 
-return <Gallery/>
+      <div
+        style={
+          view !== 0
+            ? {
+                bottom: 0,
+              }
+            : {
+                bottom: "-4rem",
+              }
+        }
+        className="transition-all duration-500 h-16 flex absolute z-20"
+      >
+        <div
+          onClick={() => setView(1)}
+          className="h-full relative aspect-[4/5]"
+        >
+          <Image
+            fill={true}
+            objectFit="contain"
+            src="https://i.ibb.co/NSbfjRs/mendezmendezart-1837138020157337605.jpg"
+            alt="galeria images"
+            className=""
+          />
+        </div>
+        <div
+          onClick={() => setView(2)}
+          className="h-full relative aspect-[5/5]"
+        >
+          <Image
+            fill={true}
+            objectFit="contain"
+            src="https://i.ibb.co/JmRyybN/Luca-Ponsato-Art-1838233197378785500.jpg"
+            alt="galeria images"
+            className=""
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
