0be15fe79101526d9d85357f1d48b7e82f0f9ffb
tohir <tohirbabs@gmail.com>
2024-09-26 18:22:58 +0100

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,14 @@ import GaleriaImg from "@/components/GaleriaImg";
 import Image from "next/image";
 import { useState } from "react";
 
+export type imgData = {
+  index: number;
+  medium: string;
+  sharp: string;
+  ARheight: number;
+  ARwidth: number;
+};
+
 export type SearchResult = { public_id: string };
 export default function Home() {
   const [view, setView] = useState(0);
@@ -12,7 +20,7 @@ export default function Home() {
   //   .max_results(50)
   //   .execute()) as { resources: SearchResult[] };
 
-  const galleryData = [
+  const galleryData1 = [
     {
       index: 1,
       medium:
@@ -39,55 +47,128 @@ export default function Home() {
     },
   ];
 
-  function sumAspectRatios(index: number) {
+  const galleryData2 = [
+    {
+      index: 4,
+      medium: "https://i.ibb.co/QQGPV4H/Ifihas-R5-1838208305547649039.jpg",
+      sharp: "https://i.ibb.co/bmnsVkv/Ifihas-R5-1838208305547649039.jpg",
+      ARheight: 56,
+      ARwidth: 71,
+    },
+    {
+      index: 5,
+      medium:
+        "https://i.ibb.co/JmRyybN/Luca-Ponsato-Art-1838233197378785500.jpg",
+      sharp:
+        "https://i.ibb.co/k5g99rp/Luca-Ponsato-Art-1838233197378785500.jpg",
+      ARheight: 1,
+      ARwidth: 1,
+    },
+    {
+      index: 6,
+      medium:
+        "https://i.ibb.co/NSbfjRs/mendezmendezart-1837138020157337605.jpg",
+      sharp: "https://i.ibb.co/xHdwjZD/mendezmendezart-1837138020157337605.jpg",
+      ARheight: 5,
+      ARwidth: 4,
+    },
+  ];
+
+  function sumAspectRatios(index: number, array: imgData[]) {
     // Ensure index is within the array bounds
-    // index = Math.min(index, galleryData.length - 1);
+    // index = Math.min(index, galleryData1.length - 1);
 
     // Calculate the sum of fractions
     console.log(index);
+    if (index > array.length) {
+      index = index - array.length;
+    }
 
     if (index === 1) {
       return 0;
     }
     let sum = 0;
     for (let i = 1; i < index; i++) {
-      sum += galleryData[i].ARheight / galleryData[i].ARwidth;
+      sum += array[i - 1].ARheight / array[i - 1].ARwidth;
+      console.log(i, array[i - 1].ARheight, array[i - 1].ARwidth);
     }
+
+    console.log(sum);
+
     return sum;
   }
 
   return (
     <div className="w-screen h-screen relative overflow-hidden">
-      <div className="relative">
-        {galleryData.map((data) => (
-          <div
-            key={data.index}
-            style={
-              view === data.index
-                ? {
-                    top: 0,
-                    left: 0,
-                    height: "100vh",
-                    width: "100vw",
-                    zIndex: 10,
-                    opacity: 1,
-                  }
-                : {
-                    top: `calc(20vw*${sumAspectRatios(data.index)})`,
-                    left: 0,
-                    height: `calc(20vw * ${data.ARheight / data.ARwidth})`,
-                    width: "20vw",
-                  }
-            }
-            className={`${
-              view === 0 ? "opacity-100" : "opacity-0"
-            } flex items-center transition-all duration-500 max-h-screen bg-black absolute`}
-          >
-            <GaleriaImg view={view} setView={setView} imgData={data} />
-          </div>
-        ))}
+      <div className="relative  w-screen h-screen">
+        <div className="relative w-[20vw]">
+          {galleryData1.map((data) => (
+            <div
+              key={data.index}
+              style={
+                view === data.index
+                  ? {
+                      top: 0,
+                      left: 0,
+                      height: "100vh",
+                      width: "100vw",
+                      zIndex: 10,
+                      opacity: 1,
+                    }
+                  : {
+                      top: `calc(20vw*${sumAspectRatios(
+                        data.index,
+                        galleryData1
+                      )})`,
+                      left: 0,
+                      height: `calc(20vw * ${data.ARheight / data.ARwidth})`,
+                      width: "20vw",
+                    }
+              }
+              className={`${
+                view === 0 ? "opacity-100" : "opacity-0"
+              } flex items-center transition-all duration-200 max-h-screen bg-black absolute`}
+            >
+              <GaleriaImg view={view} setView={setView} imgData={data} />
+            </div>
+          ))}
+        </div>
+        <div
+          style={{ left: view > 3 ? "0" : "20vw" }}
+          className="absolute w-[20vw] transition-all duration-200"
+        >
+          {galleryData2.map((data) => (
+            <div
+              key={data.index}
+              style={
+                view === data.index
+                  ? {
+                      top: 0,
+                      left: 0,
+                      height: "100vh",
+                      width: "100vw",
+                      zIndex: 10,
+                      opacity: 1,
+                    }
+                  : {
+                      top: `calc(20vw * ${sumAspectRatios(
+                        data.index,
+                        galleryData2
+                      )})`,
+                      left: 0,
+                      height: `calc(20vw * ${data.ARheight / data.ARwidth})`,
+                      width: "20vw",
+                    }
+              }
+              className={`${
+                view === 0 ? "opacity-100" : "opacity-0"
+              } flex items-center transition-all duration-200 max-h-screen bg-black absolute`}
+            >
+              <GaleriaImg view={view} setView={setView} imgData={data} />
+            </div>
+          ))}
+        </div>
       </div>
-
       <div
         style={
           view !== 0
@@ -98,9 +179,9 @@ export default function Home() {
                 bottom: "-4rem",
               }
         }
-        className="transition-all duration-500 h-16 flex absolute z-20"
+        className="transition-all duration-200 h-16 flex absolute z-20"
       >
-        {galleryData.map((data) => (
+        {galleryData1.map((data) => (
           <div
             key={data.index}
             onClick={() => setView(data.index)}
