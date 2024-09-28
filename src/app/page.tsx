899126d9bbea418f724d13a68d617b71fe032c1d
tohir_dev <devtohir@gmail.com>
2024-09-28 22:13:36 +0000

    medium img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,8 +75,7 @@ export default function Home() {
     {
       index: 7,
       medium:
-        "https://i.ibb.co/JmRyybN/Luca-Ponsato-Art-1838233197378785500.jpg",
-      sharp:
+        "https://i.ibb.co/cLTdLCq/blankspac-e-1838030863583613182.jpg",     sharp:
         "https://i.ibb.co/L5pf5JK/blankspac-e-1838030863583613182.jpg",
       ARheight: 1,
       ARwidth: 1,
