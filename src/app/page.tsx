baaa5c39d49cba65fca5a1ebcfe76040cc9946ea
devtohir <devtohir@gmail.com>
2024-10-09 17:04:28 +0000

    page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,34 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="w-screen h-screen flex p-2 gap-2">
-      <div className="mx-auto h-full flex-col justify-center flex">
-        <div className="flex flex-col relative overflow-hidden h-[650px] w-[300px] border border-[10px] rounded-[50px] bg-black border-white/40">
-          <Image fill={true} src="/assets/minimal1.png" alt="wallpaper" />
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex gap-6">
+        
+        <div className="relative h-[650px] w-[300px] rounded-[50px] overflow-hidden ">
+        
+          <Image fill={true} src="/assets/matrix.png" alt="wallpaper" />
+        </div><div className="relative h-[650px] w-[300px] rounded-[50px] overflow-hidden ">
+        
+        <Image fill={true} src="/assets/gameboy.png" alt="wallpaper" />
+      </div><div className="relative h-[650px] w-[300px] rounded-[50px] overflow-hidden ">
+        
+        <Image fill={true} src="/assets/code.png" alt="wallpaper" />
+      </div>
+      </div>
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col relative overflow-hidden h-[650px] w-[300px] outline outline-[8px] rounded-[50px] outline-black/50">
+          {/* <Image fill={true} src="/assets/punisher.png" alt="wallpaper" /> */}
           <div className="z-10 flex-col h-full justify-between items-center flex">
-          <div className="flex flex-col items-center ">
-            <div className="h-10"></div>
-            <div className="flex flex-col mx-auto text-white gap-3 text-center ">
-              <p className="font-semibold ">Friday 05 July</p>
-              <p className=" text-6xl">12:41</p>
+            <div className="flex flex-col items-center ">
+              <div className="h-10"></div>
+              <div className="flex flex-col mx-auto text-white gap-3 text-center ">
+                <p className="font-semibold ">Friday 05 July</p>
+                <p className=" text-7xl">12:41</p>
+              </div>
+            </div>
+            <div className="p-3">
+              <div className="h-[6px] w-[120px] rounded-full bg-[#7E7E7E]"></div>
             </div>
           </div>
-          <div className="p-3">
-            <div className="h-[6px] w-[120px] rounded-full bg-[#7E7E7E]"></div>
-          </div></div>
         </div>
       </div>
     </div>
