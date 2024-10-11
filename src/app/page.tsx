fde5d5599ae7b0f3034c45a9c2c5eee0c1453e64
devtohir <devtohir@gmail.com>
2024-10-11 16:00:55 +0000

    simple layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,36 @@ import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { EffectCoverflow, Navigation, Pagination } from "swiper/modules";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const [dateTime, setDateTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDateTime(new Date());
+    }, 1000); // Update every second
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const formattedDate = dateTime.toLocaleDateString("en-US", {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  });
+
+  const formattedTime = dateTime.toLocaleTimeString("en-US", {
+    hour12: false,
+    hour: "numeric",
+    minute: "numeric",
+  });
+
   return (
-    <div className="w-screen h-screen flex relative items-center justify-center  p-2 gap-2">
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <div className="flex flex-col relative bg-black overflow-hidden h-[650px] w-[300px] outline outline-[8px] rounded-[50px] outline-black/50">
+    <div className="w-screen h-screen relative">
+       
+        <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 flex flex-col relative bg-black overflow-hidden h-[650px] w-[300px] shadow-xl shadow-black/60 outline outline-[5px] rounded-[50px] outline-[#c0c0c0] border border-[5px] rounded-[50px] border-black">
           <Swiper
             effect="coverflow"
             grabCursor={true}
@@ -29,7 +53,7 @@ export default function Home() {
             }}
             modules={[EffectCoverflow, Pagination, Navigation]}
             className="swiper"
-            style={{position:"absolute"}}
+            style={{ position: "absolute" }}
           >
             <SwiperSlide className="swiper-slide">
               <Image
@@ -54,7 +78,7 @@ export default function Home() {
                 src="/assets/wave.png"
                 alt="wallpaper"
               />
-            </SwiperSlide>{" "}
+            </SwiperSlide>
             <SwiperSlide className="swiper-slide">
               <Image
                 width={300}
@@ -62,7 +86,7 @@ export default function Home() {
                 src="/assets/sciencesquibbles.png"
                 alt="wallpaper"
               />
-            </SwiperSlide>{" "}
+            </SwiperSlide>
             <SwiperSlide className="swiper-slide">
               <Image
                 width={300}
@@ -70,7 +94,7 @@ export default function Home() {
                 src="/assets/minimal1.png"
                 alt="wallpaper"
               />
-            </SwiperSlide>{" "}
+            </SwiperSlide>
             <SwiperSlide className="swiper-slide">
               <Image
                 width={300}
@@ -80,19 +104,35 @@ export default function Home() {
               />
             </SwiperSlide>
           </Swiper>
-          <div className="z-10 flex-col justify-between items-center flex">
+          <div className="z-10 flex-col w-full justify-between items-center flex">
             <div className="flex flex-col items-center ">
               <div className="h-10"></div>
               <div className="flex flex-col mx-auto text-white gap-3 text-center ">
-                <p className="font-semibold ">Friday 05 July</p>
-                <p className=" text-7xl">12:41</p>
+                <p className="font-semibold ">{formattedDate}</p>
+                <p className=" text-7xl">{formattedTime}</p>
               </div>
             </div>
-            {/* <div className="p-3">
-              <div className="h-[6px] w-[120px] rounded-full bg-[#7E7E7E]"></div>
-            </div> */}
+            <div className="p-6 absolute bottom-0 w-full flex justify-between items-center gap-2">
+              <Button
+                variant="secondary"
+                className="w-14 h-14 rounded-full bg-[#7e7e7e]/30 backdrop-blur-sm "
+              >
+                <p className="text-xl">👾</p>
+              </Button>
+              {/* <div className="h-[6px] w-[120px] rounded-full bg-[#7E7E7E]"></div> */}
+              <Button
+                variant="secondary"
+                className="w-14 h-14 rounded-full bg-[#7e7e7e]/30 backdrop-blur-sm "
+              >
+                <Image
+                  src="/assets/icons/download.png"
+                  width={30}
+                  height={30}
+                  alt="download icon"
+                />
+              </Button>
+            </div>
           </div>
-        </div>
       </div>
     </div>
   );
